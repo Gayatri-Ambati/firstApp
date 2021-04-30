be907089b40096bdf5546818588302b529290953
AMBATI GAYATRI <gayatriambati992@gmail.com>
2021-04-30 09:31:41 +0530

    resume added

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get("/", function(req, res){
     res.send("Welcome to My Basic Site");
 })
  
+app.get("/resume", function(req, res){
+    filePathName=__dirname+"/resume.html"
+    res.sendFile(filePathName);
+})
+
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
  
