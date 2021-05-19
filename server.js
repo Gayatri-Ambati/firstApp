8b453f232786d53f0f0b9456b1598ebe36f2125d
AMBATI GAYATRI <gayatriambati992@gmail.com>
2021-05-19 09:03:09 +0530

    added /crudd

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
+const courselib = require('./backend/lib/courselib');
+
+var pswd = process.env.mongoosepswd;
+console.log(pswd);
+ app.use(express.urlencoded({extended: true}));
+ app.use(express.json());
  
 app.use(express.static(path.join(__dirname, "static")));
+
+var connection_string = "mongodb+srv://library:library@cluster0.xo1ph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(connection_string, { useFindAndModify: false });
+var db = mongoose.connection;
+db.on('connected', function () {
+console.log('MongoDB connected!');
+});
+
+db.on('error', function (error) {
+console.error('Error in MongoDb connection: ' + error);
+});
+
+db.on('disconnected', function () {
+console.log('MongoDB disconnected!');
+});
+
 app.get("/", function(req, res){
     res.send("Welcome to My Basic Site");
 })
@@ -42,6 +65,14 @@ app.get("/piechart", function(req, res){
     res.sendFile(filePathName);
 })
 
+app.get("/crudd", function(req, res){
+    filePathName=__dirname+'/static/html/crud.html';
+    res.sendFile(filePathName);
+})
+app.get("/crud", courselib.getall);
+app.delete("/crud/:idd", courselib.deleteone);
+app.put("/crud/:idd", courselib.update);
+app.post("/crud",courselib.addnewone);
 // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
  
@@ -49,3 +80,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
     console.log("Server Starting running on http://localhost:"+PORT);
 })
+
