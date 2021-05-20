869322bc97355a419b7fa8c7ceb175d2e5eb7fa2
AMBATI GAYATRI <gayatriambati992@gmail.com>
2021-05-20 18:44:42 +0530

    added config file

--- server.js
+++ server.js
@@ -3,28 +3,15 @@ const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
 const courselib = require('./backend/lib/courselib');
+const connect = require('./backend/lib/dbconnect');
+const config = require('./backend/config/config');
 
-var pswd = process.env.mongoosepswd;
-console.log(pswd);
  app.use(express.urlencoded({extended: true}));
  app.use(express.json());
  
 app.use(express.static(path.join(__dirname, "static")));
 
-var connection_string = "mongodb+srv://library:library@cluster0.xo1ph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-mongoose.connect(connection_string, { useFindAndModify: false });
-var db = mongoose.connection;
-db.on('connected', function () {
-console.log('MongoDB connected!');
-});
-
-db.on('error', function (error) {
-console.error('Error in MongoDb connection: ' + error);
-});
-
-db.on('disconnected', function () {
-console.log('MongoDB disconnected!');
-});
+connect.connect();
 
 app.get("/", function(req, res){
     res.send("Welcome to My Basic Site");
@@ -74,7 +61,7 @@ app.delete("/crud/:idd", courselib.deleteone);
 app.put("/crud/:idd", courselib.update);
 app.post("/crud",courselib.addnewone);
 // Heroku will automatically set an environment variable called PORT
-const PORT = process.env.PORT || 3000;
+const PORT = config.webPort;
  
 // Start the server
 app.listen(PORT, function(){
